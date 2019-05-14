7f39112c724b2f5ed13c441af102c9d666c5e068
olbender <olbender@users.noreply.github.com>
2019-05-14 08:06:31 +0200

    Added controller

--- src/opendlv-logic-pathfollower-twopointmodel.cpp
+++ src/opendlv-logic-pathfollower-twopointmodel.cpp
@@ -29,25 +29,37 @@ int32_t main(int32_t argc, char **argv)
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
   if ( (0 == commandlineArguments.count("cid")) ||
        (0 == commandlineArguments.count("freq")) ||
-       (0 == commandlineArguments.count("rec-path")) ) {
+       (0 == commandlineArguments.count("rec-path")) ||
+       (0 == commandlineArguments.count("max-preview-distance")) ||
+       (0 == commandlineArguments.count("time-to-align")) ||
+       (0 == commandlineArguments.count("time-to-arrive")) ) {
     std::cerr << argv[0] << " follows a GPS path from the given .rec file by"
       << "using the Two point model of steering (Salvucci and Gray, 2004)."
       << std::endl
       << "Usage:   " << argv[0] << " --cid=<CID> --freq=<Frequency to send> "
       << "--rec-path=<File where the GPS path is stored> "
+      << "--max-preview-distance=<Max preview distance> "
+      << "--time-to-align=<Time-to-align the preview point in yaw angle> "
+      << "--time-to-arrive=<Time-to-arrive to preview point>" 
       << "[--id-input=<Sender stamp of GPS input>]"
-      << "[--id-output=<Sender stamp of motion request output>] [--verbose]" 
+      << "[--id-output=<Sender stamp of motion request output>] [--verbose]"
       << std::endl
       << "Example: " << argv[0] << " --cid=111 --freq=20 "
-      << "--rec-path=gps-path.rec" << std::endl;
+      << "--rec-path=gps-path.rec --max-preview-distance=20.0 "
+      << "--time-to-align=2.5 --time-to-arrive=30.0" << std::endl;
   } else {
     uint32_t const senderStampInput{
       (commandlineArguments.count("id-input") != 0) ?
         static_cast<uint32_t>(std::stoi(commandlineArguments["id-input"])) : 0};
-    //uint32_t const senderStampOutput{
-    //  (commandlineArguments.count("id-output") != 0) ?
-    //    static_cast<uint32_t>(std::stoi(commandlineArguments["id-output"])) : 0};
+    uint32_t const senderStampOutput{
+      (commandlineArguments.count("id-output") != 0) ?
+        static_cast<uint32_t>(std::stoi(commandlineArguments["id-output"])) : 0};
     bool const verbose{commandlineArguments.count("verbose") != 0};
+
+    float maxPreviewDistance{
+      std::stof(commandlineArguments["max-preview-distance"])};
+    float timeToAlign{std::stof(commandlineArguments["time-to-align"])};
+    float timeToArrive{std::stof(commandlineArguments["time-to-arrive"])};
 
     cluon::OD4Session od4{
       static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
@@ -66,6 +78,8 @@ int32_t main(int32_t argc, char **argv)
             auto msg =
               cluon::extractMessage<opendlv::proxy::GeodeticWgs84Reading>(
                   std::move(env));
+
+            od4.send(msg, cluon::time::now(), 99);
 
             std::array<double, 2> pos{msg.latitude(), msg.longitude()};
             if (!globalPath.empty()) {
@@ -104,7 +118,8 @@ int32_t main(int32_t argc, char **argv)
     int32_t closestGlobalPointIndex{-1};
 
     auto onGeodeticWgs84Reading{[&od4, &prevPos, &hasPrevPos, &globalPath, 
-      &closestGlobalPointIndex, &globalPathIsClosed, &senderStampInput, 
+      &closestGlobalPointIndex, &globalPathIsClosed, &maxPreviewDistance,
+      &timeToAlign, &timeToArrive, &senderStampInput, &senderStampOutput, 
       &verbose](cluon::data::Envelope &&envelope)
       {
         if (envelope.senderStamp() == senderStampInput) {
@@ -112,6 +127,8 @@ int32_t main(int32_t argc, char **argv)
             cluon::extractMessage<opendlv::proxy::GeodeticWgs84Reading>(
                 std::move(envelope));
           std::array<double, 2> pos{msg.latitude(), msg.longitude()};
+            
+          od4.send(msg, cluon::time::now(), 0);
 
           // Step 1: Find heading based on two positions
           if (!hasPrevPos) {
@@ -119,8 +136,11 @@ int32_t main(int32_t argc, char **argv)
             hasPrevPos = true;
             return;
           }
-          std::array<double, 2> direction = wgs84::toCartesian(prevPos, pos);
-          double heading = atan2(direction[1], direction[0]);
+          double heading;
+          {
+            std::array<double, 2> direction = wgs84::toCartesian(prevPos, pos);
+            heading = atan2(direction[1], direction[0]);
+          }
 
           // Step 2: Find global point closest to the current position
           int32_t closestPointIndex = -1;
@@ -168,9 +188,6 @@ int32_t main(int32_t argc, char **argv)
           prevPos = pos;
 
           // Step 3: Find what direction to go, based on heading
-          std::vector<std::array<double, 2>> upcomingGlobalPath;
-          upcomingGlobalPath.push_back(globalPath[closestPointIndex]);
-
           double j0Heading;
           double j1Heading;
           {
@@ -195,9 +212,11 @@ int32_t main(int32_t argc, char **argv)
           bool goingBackwards = 
             (fabs(heading - j0Heading) < fabs(heading - j1Heading));
 
-          // Step 4: Extract upcoming global path
-          double distanceToAdd = 100.0;
-          for (int32_t i = 1; distanceToAdd > 0.0; ++i) {
+          // Step 4: Find aim point
+          std::array<double, 2> aimPoint = globalPath[closestPointIndex];
+          double aimPointAngle{};
+          double aimPointDistance{};
+          for (int32_t i = 1;; ++i) {
             int32_t j;
             if (goingBackwards) {
               j = closestPointIndex - i;
@@ -211,26 +230,61 @@ int32_t main(int32_t argc, char **argv)
                   : globalPath.size() - 1;
               }
             }
-            
-            upcomingGlobalPath.push_back(globalPath[j]);
-            auto c = wgs84::toCartesian(upcomingGlobalPath[i-1],
-                upcomingGlobalPath[i]);
-            distanceToAdd -= sqrt(c[0] * c[0] + c[1] * c[1]);
-            if (!globalPathIsClosed && ((goingBackwards && j == 0) 
-                || (!goingBackwards 
-                  && j == static_cast<int32_t>(globalPath.size()) - 1))) {
-              if (verbose) {
-                std::cout << "Reached the end of the global path." 
-                  << std::endl;
+
+            double distance;
+            {
+              auto c = wgs84::toCartesian(aimPoint, globalPath[j]);
+              distance = aimPointDistance + sqrt(c[0] * c[0] + c[1] * c[1]);
+              if (distance > maxPreviewDistance) {
+                break;
               }
-              break;
+            }
+
+            double angle;
+            {
+              double const pi = 3.1415926535;
+              auto direction = wgs84::toCartesian(pos, globalPath[j]);
+              angle = atan2(direction[1], direction[0]) - heading;
+              while (angle < -pi) {
+                angle += 2.0 * pi;
+              }
+              while (angle > pi) {
+                angle -= 2.0 * pi;
+              }
+            }
+
+            aimPoint = globalPath[j];
+            aimPointAngle = angle;
+            aimPointDistance = distance;
+
+            {
+              if (!globalPathIsClosed && ((goingBackwards && j == 0) 
+                  || (!goingBackwards 
+                    && j == static_cast<int32_t>(globalPath.size()) - 1))) {
+                if (verbose) {
+                  std::cout << "Reached the end of the global path." 
+                    << std::endl;
+                }
+                break;
+              }
             }
           }
 
-          // Step 5: Transform into local frame
+          opendlv::proxy::GeodeticWgs84Reading wgs84;
+          wgs84.latitude(aimPoint[0]);
+          wgs84.longitude(aimPoint[1]);
+          od4.send(wgs84, cluon::time::now(), 98);
 
 
+          // Step 5: Calculate and send control
+          double vx = aimPointDistance / timeToArrive;
+          double yawRate = timeToAlign * aimPointAngle;
 
+          opendlv::proxy::GroundMotionRequest gmr;
+          gmr.vx(static_cast<float>(vx));
+          gmr.yawRate(static_cast<float>(yawRate));
+
+          od4.send(gmr, cluon::time::now(), senderStampOutput);
         }
       }};
     
